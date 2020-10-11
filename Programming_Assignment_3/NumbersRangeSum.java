48ac2223837fd67f7c1d6faa7bd230ae840a8b24
Alex Albright <alexanderjalbright@gmail.com>
2020-10-11 12:14:39 -0400

    Completed NumbersRangeSum

--- Programming_Assignment_3/NumbersRangeSum.java
+++ Programming_Assignment_3/NumbersRangeSum.java
@@ -1,18 +1,18 @@
-public class TriangleTest {
+public class NumbersRangeSum {
     public static void main(String[] args) { // start of program
         try {
             // Takes the command-line inputs provided (args)
             // validates them, and returns them in a useable format
             // which is an array of integers.
-            int[] sides = GetUserInputs(args);
+            int[] range = GetUserInputs(args);
 
             // Determines if the inputs are a triangle
             // by checking if any one input is greater than or equal
             // to the sum of the other two.
-            Boolean result = IsTriangular(sides[0], sides[1], sides[2]);
+            int sum = getSum(range[0], range[1]);
 
             // Print to command-line, based on result
-            DisplayResult(result);
+            DisplayResult(range, sum);
 
         } catch (NumberFormatException exception) {
             // Intercept any NumberFormatException exceptions from arguments
@@ -36,22 +36,28 @@ public class TriangleTest {
     }
 
     private static int[] GetUserInputs(String[] commandLineInputs) {
-        final int NUMBER_OF_INPUTS = 3;
+        final int NUMBER_OF_INPUTS = 2;
         // Verifies there are the correct number of command-line arguments
         if (commandLineInputs.length != NUMBER_OF_INPUTS) {
             // If not throw exception to be displayed to user, and terminate program
-            throw new IllegalArgumentException("You must enter three natural numbers. Try again.");
+            throw new IllegalArgumentException("You must enter two natural numbers. Try again.");
         }
 
         // Create place to hold numbers provided by user from the command-line
         int[] inputs = new int[NUMBER_OF_INPUTS];
 
-        // Loop three times to look at each provieded command-line input
+        // Loop two times to look at each provieded command-line input
         for (int i = 0; i < NUMBER_OF_INPUTS; i++) {
             // Parse String input as an integer and save into variable to hold values
             inputs[i] = Integer.parseInt(commandLineInputs[i]);
             // Perform validation on inputs
             ValidateInput(inputs[i]);
+        }
+
+        // Verify first number is less than second number
+        if (inputs[0] >= inputs[1]) {
+            // If not throw exception to be displayed to user, and terminate program
+            throw new IllegalArgumentException("The second number provided must be greater than the first.");
         }
 
         // Send array of integers back to main to be used in next step
@@ -69,23 +75,25 @@ public class TriangleTest {
         }
     }
 
-    private static Boolean IsTriangular(int side1, int side2, int side3) {
-        // Check to see if any one input is equal to or greater than
-        // the sum of the other two inputs
-        if (side1 >= side2 + side3 || side2 >= side1 + side3 || side3 >= side2 + side1) {
-            // Since not a triangle, send false back to calling method (Main) to be used in
-            // next step
-            return false;
+    private static int getSum(int rangeStart, int rangeEnd) {
+        // Create place to store accumulated sum
+        int sum = 0;
+        // Loop from start of range through each natural number
+        // until the end of the range
+        for (int i = rangeStart; i <= rangeEnd; i++) {
+            // Add each natural number to the accumulated sum
+            sum += i;
         }
-
-        // Since it is a triangle, send true back to calling method (Main) to be used in
-        // next step
-        return true;
+        // Send value back to Main to be used in next step
+        return sum;
     }
 
-    private static void DisplayResult(Boolean result) {
-        // Prints True if the inputs are a triangle
-        // False if they are not
-        System.out.println(result ? "True" : "False");
+    private static void DisplayResult(int[] range, int sum) {
+        // Create interpolated string message with range and sum.
+        String resultMessage = String.format("The sum of natural numbers in range [%s,%s] is %s.", range[0], range[1],
+                sum);
+        // Prints the resulting statement showing
+        // the range given, and the sum calculated.
+        System.out.println(resultMessage);
     }
 }
